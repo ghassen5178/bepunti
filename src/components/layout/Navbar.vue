6c6a39f8694d5bc73e5531b1dad0010d91b8070a
ghassen5178 <136532936+ghassen5178@users.noreply.github.com>
2023-09-04 10:46:25 +0200

    Update Navbar.vue

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -66,7 +66,7 @@ export default {
               src="../../assets/favicon.svg"
               class="w-12 rounded-full object-cover mr-2"
               alt="logo" />
-            <span class="text-3xl font-bold">BeFake</span>
+            <span class="text-3xl font-bold">Bepunti</span>
           </router-link>
         </div>
         <div class="hidden md:flex items-center">
